038db00aa0b654dae701729679b674c2f428913a
omen <kybar089@gmail.com>
2023-01-26 21:53:22 -0600

    updated editor.js to fix .split error

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -25,8 +25,10 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
+      const dataString = data.map(d => d.value).join("\n")
+      console.log(data)
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      this.editor.setValue(dataString || localData || header);
     });
 
     this.editor.on('change', () => {
